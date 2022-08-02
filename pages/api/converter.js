0120f447f2c1e3c4fb7b1fbc4f87f67ae8be4483
MartinPettersen <searchingtheskyfor@gmail.com>
2022-08-02 13:24:21 +0200

    add tab functionality to textarea

--- pages/api/converter.js
+++ pages/api/converter.js
@@ -1,63 +1,58 @@
-
-
-
-
-
 const functionConverter = (input) => {
+  let parenthesisStack = [];
+  let doubleQuoteStack = [];
+  let SingleQuoteStack = [];
+  let backtickStack = [];
 
-    let parenthesisStack = [];
-    let doubleQuoteStack = [];
-    let SingleQuoteStack = [];
-    let backtickStack = [];
+  const matchPattern = "function";
+  const matchDoubleQuote = '"';
+  const matchSingleQuote = "'";
+  const matchBacktick = "`";
+  const matchParenthesis = "(";
+  const matchMultiLineComOp = "/*";
+  const matchMultiLineComClo = "*/";
 
-    const matchPattern = 'function';
-    const matchDoubleQuote = '"';
-    const matchSingleQuote = "'";
-    const matchBacktick = '`';
-    const matchParenthesis = '(';
+  const matchPaternLength = matchPattern.length;
+  const inputLength = input.length;
 
+  let response;
+  //--------------------------------------------------------------------------
 
-    const matchPaternLength = matchPattern.length;
-    const inputLength = input.length;
-
-    let response
-    //--------------------------------------------------------------------------
-
-    const commentedOutChecker = () => {
-        if(doubleQuoteStack.length > 0){
-            return true;
-        }
-        return false;
+  const commentedOutChecker = () => {
+    if (doubleQuoteStack.length > 0) {
+      return true;
     }
+    return false;
+  };
 
-    const checkRemainingLength = (i, matchPaternLength, inputLength ) => {
-        if ( (i + matchPaternLength) <= inputLength) {
-            return true;
-        }
-        return false
+  const checkRemainingLength = (i, matchPaternLength, inputLength) => {
+    if (i + matchPaternLength <= inputLength) {
+      return true;
     }
+    return false;
+  };
 
-    const removeFunction = (i) => {
-        const beforeText = input.substring(0, i);
-        const afterText = input.substring(i + matchPaternLength +1);
-        const afterTextWithArrow = insertArrow(i, afterText);
-        return beforeText + afterTextWithArrow;
-    }
+  const removeFunction = (i) => {
+    const beforeText = input.substring(0, i);
+    const afterText = input.substring(i + matchPaternLength + 1);
+    const afterTextWithArrow = insertArrow(i, afterText);
+    return beforeText + afterTextWithArrow;
+  };
 
-    const insertArrow = (i, rem) => {
-        //const remaining = input.substring(i + matchPaternLength +1);
-        //console.log(remaining);
-        //const beforeText = rem.substring(0, i+1);
-        //console.log(beforeText);
+  const insertArrow = (i, rem) => {
+    //const remaining = input.substring(i + matchPaternLength +1);
+    //console.log(remaining);
+    //const beforeText = rem.substring(0, i+1);
+    //console.log(beforeText);
 
-        //let tempString = ""
-        //console.log(rem);
-        const regExp = /(\([^]+\))/;
-        //const matches = regExp.exec(rem);
-        const match = rem.replace(regExp, "$1 =>");
-        //console.log(match)
-        return match;
-        /*
+    //let tempString = ""
+    //console.log(rem);
+    const regExp = /(\([^]+\))/;
+    //const matches = regExp.exec(rem);
+    const match = rem.replace(regExp, "$1 =>");
+    //console.log(match)
+    return match;
+    /*
         for (let i = 0; i < remaining.length; i++){
             
             tempString += remaining[i];
@@ -75,53 +70,42 @@ const functionConverter = (input) => {
             }
         }
         */
+  };
+  // ------------------------------------------------------------------------
+
+  for (let i = 0; i < inputLength; i++) {
+    if (input[i] === matchDoubleQuote) {
+      if (commentedOutChecker(doubleQuoteStack)) {
+        doubleQuoteStack.pop();
+      } else {
+        doubleQuoteStack.push('"');
+      }
     }
-    // ------------------------------------------------------------------------
 
-
-    for (let i = 0; i < inputLength; i++)
-    {
-        
-        if(input[i] === matchDoubleQuote){
-
-            if(commentedOutChecker(doubleQuoteStack)){
-                doubleQuoteStack.pop();
-            } else {
-                doubleQuoteStack.push('"');
-            }
-        };
-
-        if(input[i] === matchPattern[0] && !commentedOutChecker()){
-            if(checkRemainingLength(i, matchPaternLength, inputLength)){
-                //console.log(input[i]);
-                //console.log(i);
-                //console.log(matchPaternLength);
-                //console.log(inputLength);
-                //console.log(input[(i + matchPaternLength -1)]);
-
-                if (input[i + matchPaternLength -1] === matchPattern[matchPaternLength -1]) {
-                    response = "front and end match";
-                    response = removeFunction(i);
-                }
-
-            } else {
-                response = "string to short to contain function";
-                break;
-            }
-
+    if (input[i] === matchPattern[0] && !commentedOutChecker()) {
+      if (checkRemainingLength(i, matchPaternLength, inputLength)) {
+        if (
+          input[i + matchPaternLength - 1] ===
+          matchPattern[matchPaternLength - 1]
+        ) {
+          response = "front and end match";
+          response = removeFunction(i);
         }
-
-    }
-    return response;
-}
-
-export default function handler(req, res) {
-    if (req.method === 'POST') {
-        const input = req.body.input.text;
-        const matchNewline = /\r|\n/.exec(input);
-//        console.log(matchNewline);
-
-        const response = functionConverter(input);
-        res.status(200).json({ result: `${response}` });
+      } else {
+        response = "string to short to contain function";
+        break;
+      }
     }
   }
+  return response;
+};
+
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const input = req.body.input.text;
+    //      const matchNewline = /\r|\n/.exec(input);
+
+    const response = functionConverter(input);
+    res.status(200).json({ result: `${response}` });
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,48 @@
-import axios from 'axios';
-import { setRevalidateHeaders } from 'next/dist/server/send-payload';
-import Head from 'next/head'
-import Image from 'next/image'
-import { useState } from 'react'
-import CodeText from '../components/CodeText';
-import styles from '../styles/Home.module.css'
-export default function Home() {
+import axios from "axios";
+import { setRevalidateHeaders } from "next/dist/server/send-payload";
+import Head from "next/head";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import CodeText from "../components/CodeText";
+import styles from "../styles/Home.module.css";
 
+export default function Home() {
   const [before, setBefore] = useState("");
   const [after, setAfter] = useState("");
 
-  const handleCopy =  async() => {
+  const handleCopy = async () => {
     return await navigator.clipboard.writeText(after);
+  };
+
+  const handleTab = (e) => {
+    //console.log(e)
+    console.log(e.target.selectionEnd)
+    console.log(e.target.selectionStart)
+
+
+    if (e.key == 'Tab') {
+      e.preventDefault();
+      const start = before.substring(0, e.target.selectionStart);
+      const end = before.substring(e.target.selectionEnd);
+
+      const insertTab = start + "\t" + end;
+      console.log(insertTab);
+      setBefore(insertTab);
+    }
   }
 
   const handleClick = () => {
+    const matchNewline = before.replace(/\r|\n/, "\n");
+    const input = { text: matchNewline };
+    axios
+      .post("/api/converter", { input })
+      .then((res) => setAfter(res.data.result));
+    // const matchNewlineAfter = after.replace(/\r|\n/, '***');
+  };
 
-      //const matchNewline = /\r|\n/.exec(before);
-      const matchNewline = before.replace(/\r|\n/, '\n');
-      
-      //console.log(matchNewline)
-
-      const input = { text: matchNewline };
-
-      axios.post('/api/converter', { input })
-      .then((res) => setAfter(res.data.result))
-      const matchNewlineAfter = after.replace(/\r|\n/, '***');
-      
-      console.log(matchNewlineAfter)
-  }
+  useEffect(() => {
+    console.log("changeed before")
+  },[before]);
 
   return (
     <div className={styles.container}>
@@ -43,32 +57,36 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          <code >Paste your oldschool JavaScript functions on the left and push the convert button</code>
+          <code>
+            Paste your oldschool JavaScript functions on the left and push the
+            convert button
+          </code>
         </p>
-        <button className={styles.convert_button} onClick={handleClick}>Convert</button>
+        <button className={styles.convert_button} onClick={handleClick}>
+          Convert
+        </button>
         <div className={styles.grid}>
           <div className={styles.card}>
-            
-            <textarea className={styles.code} onChange={(e) => setBefore(e.target.value)}></textarea>
+            <div className="whiteSpacesFix">
+
+            <textarea
+              className={styles.code}
+              value={before}
+              onKeyDown={(e) => handleTab(e)}
+              onChange={(e) => setBefore(e.target.value)}
+              ></textarea>
+              </div>
           </div>
 
           <div className={styles.card}>
-           <div className={[styles.after_container]}>
-           <button onClick={handleCopy} >Copy</button>
-             
-             <CodeText text={after} />
-           </div>
+            <div className={[styles.after_container]}>
+              <button onClick={handleCopy}>Copy</button>
 
+              <CodeText text={after} />
+            </div>
           </div>
-          
-
-
         </div>
       </main>
-
-
     </div>
-  )
+  );
 }
-
-
