fb5da4c35cc2b0eca85498d7176c67fb92efa825
MartinPettersen <searchingtheskyfor@gmail.com>
2022-08-04 09:27:12 +0200

    add download file

--- pages/filePage.js
+++ pages/filePage.js
@@ -1,10 +1,15 @@
 import React, { useState, useEffect, useRef } from "react";
+import styles from "../styles/Home.module.css";
 
 const FilePage = () => {
   const [isDragging, setIsDragging] = useState(false);
   const inputFile = useRef(null);
 
   const [selectedFile, setSelectedFile] = useState(null);
+  const [fileContent, setFileContent] = useState("");
+  const [fileName, setFileName] = useState("yourfile");
+
+
   /*
     const uploadFile = (e) => {
         setSelectedFile(e)
@@ -21,30 +26,57 @@ const FilePage = () => {
   //<input type='file' id='file' ref={inputFile} style={{display: 'none'}}/>
   const readFile = (event) => {
     const fileReader = new FileReader();
-    const  { files }  = event.target;
-    
-    console.log(typeof files)
-    
+    const { files } = event.target;
+
+    console.log(typeof files);
+
     fileReader.readAsText(files[0], "UTF-8");
     fileReader.onload = (e) => {
       const content = e.target.result;
+      setFileName(files[0].name);
       console.log(content);
+      setFileContent(content);
       //setFileContent(JSON.parse(content));
     };
-    
+
     //console.log(inputFile.current.files[0]);
+  };
+
+  const downloadTxtFile = () => {
+    const element = document.createElement("a");
+    const file = new Blob([fileContent], {
+      type: "text/plain",
+    });
+    element.href = URL.createObjectURL(file);
+
+    element.download = `${fileName}`;
+    document.body.appendChild(element);
+    element.click();
   };
 
   return (
     <div>
-      filePage
-      <input
-        type="file"
-        value={selectedFile}
-        ref={inputFile}
-        onChange={readFile}
-      />
-      <button onClick={() => inputFile.current.click()}>upload</button>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Turn your old functions in to arrow functions
+        </h1>
+        <p className={styles.description}>
+          <code>
+            Upload a file and push the
+            convert button
+          </code>
+        </p>
+        
+        <input
+          type="file"
+          value={selectedFile}
+          ref={inputFile}
+          onChange={readFile}
+        />
+        {/*<button onClick={() => inputFile.current.click()}>upload</button>*/}
+        {fileContent !== "" ? <button onClick={downloadTxtFile}>Convert</button> :<></>}
+        
+      </main>
     </div>
   );
 };
